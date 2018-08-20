707d31a6c542276dff4aab3a4eb3bfd5abd81bfc
Juan <Juan@jf-PC>
2018-08-19 23:53:27 -0600

    Correcion de la GUI y documentacion

--- POC/pruebas.py
+++ POC/pruebas.py
@@ -24,22 +24,35 @@ class TestUM(unittest.TestCase):
     #testing de los codigos de estado
     
     def test_index_status_code(self):
+        """Funcion para probar la respuesta al url: /index
+        """
         result = self.app.get('/') 
         self.assertEqual(result.status_code, 200)
     def test_crear_status_code(self):
+        """Funcion para probar la respuesta al url: /crear
+        """
         result = self.app.get('/crear') 
         self.assertEqual(result.status_code, 200)
     def test_generar_status_code(self):
+        """Funcion para probar la respuesta al url: /generar
+        """
         result = self.app.get('/generar') 
         self.assertEqual(result.status_code, 200)
     def test_predecir_status_code(self):
+        """Funcion para probar la respuesta al url: /predecir
+        """
         result = self.app.get('/predecir') 
         self.assertEqual(result.status_code, 200)
     def test_cargar_status_code(self):
+        """Funcion para probar la respuesta al url: /cargar
+        """
         result = self.app.get('/cargar') 
         self.assertEqual(result.status_code, 200)
     
     def test_predecir_modelo_response(self):
+        """Funcion para probar las funciones de prediccion
+        Simula una peticion POST en la que le envia la imagen perro.jpg
+        """
         data={}
         data['file'] = open('uploads\\perro.jpg', 'rb')
         #result = self.app.get('/cargar') 
@@ -49,6 +62,9 @@ class TestUM(unittest.TestCase):
             )
         self.assertIn(b'wall_clock', response.data)
     def test_crear_csv(self):
+        """Funcion para probar la funcion de generacion de archivos .csv
+        Simula una peticion GET
+        """
         data={}
         data['file'] = open('uploads\\perro.jpg', 'rb')
         result = self.app.get('/crear') 
