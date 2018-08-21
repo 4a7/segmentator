9d1d07f7dfa18d55c1653deb9103e0ecce28fc19
Bryan <bryanvargasf2110@github.com>
2018-08-21 00:39:57 -0600

    documentacion interna

---
+++ POC/html/search/all_0.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['test_5fcargar_5fstatus_5fcode',['test_cargar_status_code',['../classpruebas_1_1_test_u_m.html#af0e1b161b7a46c48fe74e9cda7d5cd4c',1,'pruebas::TestUM']]],
+  ['test_5fcrear_5fcsv',['test_crear_csv',['../classpruebas_1_1_test_u_m.html#aeb181c390d170aed40acc8de970abfe1',1,'pruebas::TestUM']]],
+  ['test_5fcrear_5fstatus_5fcode',['test_crear_status_code',['../classpruebas_1_1_test_u_m.html#a152091a0b91cda976f0ef41a6feef968',1,'pruebas::TestUM']]],
+  ['test_5fgenerar_5fstatus_5fcode',['test_generar_status_code',['../classpruebas_1_1_test_u_m.html#a3aa8f257e52cba3f778181f02241ce63',1,'pruebas::TestUM']]],
+  ['test_5findex_5fstatus_5fcode',['test_index_status_code',['../classpruebas_1_1_test_u_m.html#a4cecf42b50d10428f2e5c1efca0ce59c',1,'pruebas::TestUM']]],
+  ['test_5fpredecir_5fmodelo_5fresponse',['test_predecir_modelo_response',['../classpruebas_1_1_test_u_m.html#a0cda9b35bc5adad4dc7d08c1c6b4bfa8',1,'pruebas::TestUM']]],
+  ['test_5fpredecir_5fstatus_5fcode',['test_predecir_status_code',['../classpruebas_1_1_test_u_m.html#a775d549dfd6d3b2822c5e1579552f6a8',1,'pruebas::TestUM']]],
+  ['testum',['TestUM',['../classpruebas_1_1_test_u_m.html',1,'pruebas']]]
+];
---
+++ POC/html/search/classes_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['testum',['TestUM',['../classpruebas_1_1_test_u_m.html',1,'pruebas']]]
+];
